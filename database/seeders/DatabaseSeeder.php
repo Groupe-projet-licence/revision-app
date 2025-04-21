667e7640343035d6d924dd51fbea13e74694ac61
darlin00 <donfackdarlin@gmail.com>
2025-04-21 17:38:30 +0100

    Modification du fichier DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -20,14 +20,49 @@ class DatabaseSeeder extends Seeder
             "password"=> Hash::make("670748873")
         ]);
         \App\Models\User::factory()->create([
-            "name"=>"wafo",
+            "name"=>"rochelin",
             "email"=> "anoumedemrochelin6@gmail.com",
+            "password"=> Hash::make("698112522")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"wafo",
+            "email"=> "aroldwafo50@gmail.com",
             "password"=> Hash::make("676351663")
         ]);
         \App\Models\User::factory()->create([
-            "name"=>"rochelin",
-            "email"=> "wafoarold@gmail.com",
-            "password"=> Hash::make("698112522")
+            "name"=>"alicia",
+            "email"=> "ematalicia5@gmail.com",
+            "password"=> Hash::make("653654468")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"elisabeth",
+            "email"=> "kennepewoelisabeth223@gmail.com",
+            "password"=> Hash::make("654992997")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"elie",
+            "email"=> "fotsoelie52@gmail.com",
+            "password"=> Hash::make("657945736")
+        ]); 
+        \App\Models\User::factory()->create([
+            "name"=>"ange",
+            "email"=> "angekengne651@gmail.com",
+            "password"=> Hash::make("651261713")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"jason",
+            "email"=> "Atemjason11@gmail.com",
+            "password"=> Hash::make("652725764")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"john",
+            "email"=> "kuetchejohn19@gmail.com",
+            "password"=> Hash::make("675372019")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"patricia",
+            "email"=> "patricia@gmail.com",
+            "password"=> Hash::make("651641499")
         ]);
 
 
