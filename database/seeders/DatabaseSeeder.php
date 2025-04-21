5109fe788d2186403118cc8630a86e68e460faa9
darlin00 <donfackdarlin@gmail.com>
2025-04-21 16:07:06 +0100

    Modification de DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,7 @@ namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
 
 class DatabaseSeeder extends Seeder
 {
@@ -13,6 +14,22 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         // \App\Models\User::factory(10)->create();
+        \App\Models\User::factory()->create([
+            "name"=>"darlin",
+            "email"=> "donfackdarlin@gmail.com",
+            "password"=> Hash::make("670748873")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"wafo",
+            "email"=> "anoumedemrochelin6@gmail.com",
+            "password"=> Hash::make("676351663")
+        ]);
+        \App\Models\User::factory()->create([
+            "name"=>"rochelin",
+            "email"=> "wafoarold@gmail.com",
+            "password"=> Hash::make("698112522")
+        ]);
+
 
         // \App\Models\User::factory()->create([
         //     'name' => 'Test User',
