07a38d14547a35a1ff72de29fcdca111b2d85ce7
Patricia <daghengpatricia716@gmail.com>
2025-06-04 15:25:59 +0200

    modificationquizz

--- resources/js/Pages/Quizzes/QuizzesCreate.jsx
+++ resources/js/Pages/Quizzes/QuizzesCreate.jsx
@@ -1,12 +1,14 @@
 import QuillEditor from "@/Components/QuillEditor";
 import AuthLayouts from "@/Layouts/AuthLayouts";
-import { Link } from "@inertiajs/react";
 
 export default function QuizzesIndex() {
     return <AuthLayouts>
-        <div className="text-end">
-            <Link className="btn btn-primary mb-4" href='/quizzes/create'> <span className="fs-5">+</span> New quiz</Link>
+        Quizz
+        <div className="big-quill big-loader">
+
+            <QuillEditor />
         </div>
+        <QuillEditor />
 
     </AuthLayouts>
 }
